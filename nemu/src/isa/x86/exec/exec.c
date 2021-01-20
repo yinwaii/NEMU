4de987fb7d70dfddad96fc3982dd855f1ec31efd
519030910145-yinwai <yinwai@sjtu.edu.cn>
2021-01-20 15:45:15 +0800

    tongbu

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -111,6 +111,7 @@ again:
     IDEXW(0xd2, gp2_cl2E, gp2, 1)
     IDEX (0xd3, gp2_cl2E, gp2)
     EX   (0xd6, nemu_trap)
+	IDEX (0xE8, J, call)
     IDEXW(0xf6, E, gp3, 1)
     IDEX (0xf7, E, gp3)
     IDEXW(0xfe, E, gp4, 1)
