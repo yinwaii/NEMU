67d074a33e1c722a20f8f6abc4de6835054de80a
yinwai <yinwai@sjtu.edu.cn>
2021-01-25 17:27:25 +0000

    keyboard

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t key_value = inl(KBD_ADDR);
+  kbd->keydown = (key_value != AM_KEY_NONE);
+  kbd->keycode = key_value;
 }
